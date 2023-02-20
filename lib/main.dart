10421b323cbec715198633fb966c808af8ca61e1
priyasu-cx <priyasuguin.stu@gmail.com>
2023-02-20 21:32:39 +0530

    Added Temp HomePage + Configured Auto router

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
-import 'package:ccd2023/screens/login/login.dart';
+import 'package:ccd2023/router/ccd_router.gr.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const CCDApp());
+  runApp(CCDApp());
 }
 
 class CCDApp extends StatelessWidget {
 
-  const CCDApp({super.key});
+  CCDApp({super.key});
+
+  final _appRouter = AppRouter();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    // return MaterialApp(
+    //   // title: 'CCD 2023',
+    //   // debugShowCheckedModeBanner: false,
+    //   // theme: ThemeData(
+    //   //   // This is the theme of your application.
+    //   //   // Try running your application with "flutter run". You'll see the
+    //   //   // application has a blue toolbar. Then, without quitting the app, try
+    //   //   // changing the primarySwatch below to Colors.green and then invoke
+    //   //   // "hot reload" (press "r" in the console where you ran "flutter run",
+    //   //   // or simply save your changes to "hot reload" in a Flutter IDE).
+    //   //   // Notice that the counter didn't reset back to zero; the application
+    //   //   // is not restarted.
+    //   //   primarySwatch: Colors.blue,
+    //   // ),
+    //   // /// TODO: Configure Auto Router
+    //   // builder: Navigator.builder(),
+    //   // home: LoginScreen(),
+    //
+    // );
+    return MaterialApp.router(
       title: 'CCD 2023',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -27,9 +48,8 @@ class CCDApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      /// TODO: Configure Auto Router
-
-      home: LoginScreen(),
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
