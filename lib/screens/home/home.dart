db7432d9a50432f7f4893754d91d705c9b61603d
shata <shatanikmahanty@gmail.com>
2023-02-24 15:14:33 +0530

    Added Google Sans font

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,7 +7,6 @@ import 'package:ccd2023/utils/ccd_asset.dart';
 import 'package:ccd2023/utils/ccd_colors.dart';
 import 'package:ccd2023/utils/size_config.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,81 +14,77 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        top: true,
-        child: Scaffold(
-          backgroundColor: Colors.black,
-          appBar: PreferredSize(
-            preferredSize: Size.fromHeight(screenHeight! * 0.07),
-            child: DefaultAppbar(context),
-          ),
-          drawer: defaultDrawer(),
-          body: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth! * 0.08),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      top: true,
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(screenHeight! * 0.07),
+          child: DefaultAppbar(context),
+        ),
+        drawer: defaultDrawer(),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenWidth! * 0.08),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: screenWidth! * 0.13),
+                  child: Image.asset(
+                    GCCDImageAssets.googleCloudLogo,
+                    width: screenWidth! * 0.58,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Text(HomeScreenData.eventTitle,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenWidth! * 0.11,
+                      fontWeight: FontWeight.w400,
+                    )),
+                SizedBox(height: screenWidth! * 0.06),
+                RichText(
+                  textAlign: TextAlign.justify,
+                  text: TextSpan(
                     children: [
-                      Padding(
-                        padding: EdgeInsets.only(top: screenWidth! * 0.13),
-                        child: Image.asset(
-                          GCCDImageAssets.googleCloudLogo,
-                          width: screenWidth! * 0.58,
-                          fit: BoxFit.fitWidth,
-                        ),
-                      ),
-                      Text(HomeScreenData.eventTitle,
+                      TextSpan(
+                          text: HomeScreenData.eventHashTag,
                           style: TextStyle(
-                            color: Colors.white,
-                            fontSize: screenWidth! * 0.11,
-                            fontWeight: FontWeight.w400,
-                            fontFamily: GoogleFonts.inriaSans().fontFamily,
+                            color: GCCDColor.googleYellow,
+                            fontSize: screenWidth! * 0.04,
+                            fontStyle: FontStyle.normal,
                           )),
-                      SizedBox(height: screenWidth! * 0.06),
-                      RichText(
-                        textAlign: TextAlign.justify,
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                                text: HomeScreenData.eventHashTag,
-                                style: TextStyle(
-                                  color: GCCDColor.googleYellow,
-                                  fontSize: screenWidth! * 0.04,
-                                  // fontWeight: FontWeight.w200,
-                                  fontFamily: GoogleFonts.openSans().fontFamily,
-                                  fontStyle: FontStyle.normal,
-                                )),
-                            TextSpan(
-                              text: HomeScreenData.eventDescription,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: screenWidth! * 0.04,
-                                // fontWeight: FontWeight.w200,
-                                fontFamily: GoogleFonts.openSans().fontFamily,
-                                fontStyle: FontStyle.normal,
-                              ),
-                            ),
-                          ],
+                      TextSpan(
+                        text: HomeScreenData.eventDescription,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: screenWidth! * 0.04,
+                          fontStyle: FontStyle.normal,
                         ),
                       ),
+                    ],
+                  ),
+                ),
 
-                      /// TODO: Timer
+                /// TODO: Timer
 
-                      Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: screenWidth! * 0.1),
-                        child: Column(
-                          children: [
-                            defaultButton("Register Now", GCCDColor.googleBlue),
-                            defaultButton(
-                                "Call for Speakers", GCCDColor.googleGrey),
-                          ],
-                        ),
-                      ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: screenWidth! * 0.1),
+                  child: Column(
+                    children: [
+                      defaultButton("Register Now", GCCDColor.googleBlue),
+                      defaultButton("Call for Speakers", GCCDColor.googleGrey),
+                    ],
+                  ),
+                ),
 
-                      aboutSection(),
-                    ]),
-              )),
-        ));
+                aboutSection(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
