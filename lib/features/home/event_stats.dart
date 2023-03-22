4bb09017a7e3a1e3ff9f56635a158e4116d8c47e
shata <shatanikmahanty@gmail.com>
2023-03-22 23:26:49 +0530

    Fixed event stats display

--- lib/features/home/event_stats.dart
+++ lib/features/home/event_stats.dart
@@ -1,5 +1,4 @@
 import 'package:ccd2023/configurations/configurations.dart';
-import 'package:ccd2023/utils/size_util.dart';
 import 'package:djangoflow_app/djangoflow_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,12 +8,13 @@ class EventStats extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
-      padding: EdgeInsets.all(screenWidth! * 0.08),
+      padding: EdgeInsets.all(kPadding * 2),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: statData.map((e) => StatWidget(title: e['title']!, value: e['value']!)).toList(),
+        children: statData
+            .map((e) => StatWidget(title: e['title']!, value: e['value']!))
+            .toList(),
       ),
     );
   }
@@ -33,47 +33,40 @@ class StatWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final themeMode = context.watch<AppCubit>().state.themeMode;
-    return GridView(
-      physics: const NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2
+    return Padding(
+      padding: const EdgeInsets.all(
+        kPadding * 3,
       ),
-      children: [
-        Container(
-          padding: EdgeInsets.only(right: screenWidth! * 0.08),
-          alignment: Alignment.centerRight,
-          width: screenWidth! * 0.2,
-          height: screenWidth! * 0.1,
-          child: Text(
-            value,
-            style: Theme.of(context).textTheme.displayLarge?.copyWith(
-              fontWeight: FontWeight.w300,
-              fontSize: screenWidth! * 0.1,
-              // color: themeMode == ThemeMode.dark ? Colors.black : Colors.white,
-              foreground: Paint()
-                ..style = PaintingStyle.stroke
-                ..strokeWidth = 1
-                ..color = themeMode == ThemeMode.dark ?
-                Colors.white : Colors.black,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 3,
+            child: Text(
+              value,
+              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                    fontWeight: FontWeight.w300,
+                    foreground: Paint()
+                      ..style = PaintingStyle.stroke
+                      ..strokeWidth = 1
+                      ..color = themeMode == ThemeMode.dark
+                          ? Colors.white
+                          : Colors.black,
+                  ),
             ),
           ),
-        ),
-        Container(
-          alignment: Alignment.centerLeft,
-          width: screenWidth! * 0.2,
-          height: screenWidth! * 0.1,
-          child: Text(
-            title,
-            style: Theme.of(context).textTheme.titleLarge?.copyWith(
-              fontWeight: FontWeight.w600,
-              fontSize: screenWidth! * 0.09,
-              // color: themeMode == ThemeMode.dark ? Colors.black : Colors.white,
+          Expanded(
+            flex: 4,
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    fontWeight: FontWeight.w600,
+                    // color: themeMode == ThemeMode.dark ? Colors.black : Colors.white,
+                  ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
-
