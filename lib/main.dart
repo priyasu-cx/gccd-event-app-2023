22cbd886bcad59e0ef744b8e0b37432a29070257
shubhayu-64 <majumdarshubhayu@gmail.com>
2023-05-05 21:27:56 +0530

    Added Push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:ccd2023/ccd_app_builder.dart';
+import 'package:ccd2023/configurations/services/notification_service.dart';
 import 'package:ccd2023/features/auth/auth.dart';
+import 'package:ccd2023/firebase_options.dart';
 import 'package:dio/dio.dart';
 import 'package:djangoflow_app/djangoflow_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'configurations/configurations.dart';
 import 'configurations/router/auth_guard.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await NotificationService().initNotification();
+  await NotificationService().subscribeToTopic("all");
   DjangoflowAppRunner.run(
     onException: (exception, stackTrace) {
       debugPrint('Exception Caught -- $exception');
